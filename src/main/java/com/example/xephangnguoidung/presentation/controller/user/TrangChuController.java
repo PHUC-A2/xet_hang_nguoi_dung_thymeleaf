b0b6269c7d4a84f36da20316e2bfd2b6b9b67922
PHUC-A2 <phucban297@gmail.com>
2025-03-06 00:37:31 +0700

    Hoàn thiện UI TrangChu,BXH cho USER

--- src/main/java/com/example/xephangnguoidung/presentation/controller/user/TrangChuController.java
+++ src/main/java/com/example/xephangnguoidung/presentation/controller/user/TrangChuController.java
@@ -1,4 +1,12 @@
 package com.example.xephangnguoidung.presentation.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TrangChuController {
-  
+    @GetMapping("/user")
+    public String trangChu() {
+        return "user/trang_chu";
+    }
 }
